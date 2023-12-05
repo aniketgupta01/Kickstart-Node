f20c8cfd21a82cb410065ce6d2dafd659d000e8b
aniketgupta01 <aniketg2831@gmail.com>
2023-12-06 00:31:37 +0530

    cleaned up code

--- index.js
+++ index.js
@@ -1,44 +1,5 @@
-const http = require("http");
-const fs = require("fs");
-const server = http.createServer((req, res) => {
-  const url = req.url;
-  const method = req.method;
-  if (url === "/") {
-    fs.readFile("message.txt", { encoding: "utf-8" }, (err, data) => {
-      if (err) {
-        console.log(err);
-      }
+const http= require('http');
+const routs=require('./routes');
 
-      res.write("<html>");
-      res.write("<head></head>");
-      res.write(`<body>${data}</body>`);
-      res.write(
-        '<body><form action="/message" method="POST"><input type = "text" name="message"></input><button type="submit">Send</button></form></body>'
-      );
-      res.write("</html>");
-      return res.end();
-    });
-  }
-  else if (url === "/message" && method === "POST") {
-    const body = [];
-    req.on("data", (chunk) => {
-      body.push(chunk);
-    });
-    return req.on("end", () => {
-      const parsedBody = Buffer.concat(body).toString();
-      const message = parsedBody.split("=")[1];
-      fs.writeFile("message.txt", message, () => {
-        res.statusCode = 302;
-        res.setHeader("Location", "/");
-        return res.end();
-      });
-    });
-  }
-
-//   res.write("<html>");
-//   res.write("<head></head>");
-//   res.write("<body><h1>Welcome to About Us Page</h1></body>");
-//   res.write("</html>");
-//   res.end();
-});
-server.listen(4000);
+const server=http.createServer(routs);
+server.listen(3000)
