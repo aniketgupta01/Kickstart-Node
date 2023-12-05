a42712ad36200e0f76ba8c9b3a220152085c6a2a
aniketgupta01 <aniketg2831@gmail.com>
2023-12-05 13:25:50 +0530

    request/response

--- index.js
+++ index.js
@@ -1,6 +1,36 @@
-const http = require('http');
-const server = http.createServer((req, res)=>{
-    console.log('Aniket Gupta');
-
+const http= require('http');
+const server=http.createServer((req,res)=>{
+    if(req.url === '/home'){
+        res.setHeader('Content-type','text/html')
+        res.write('<html>');
+        res.write('<head></head>')
+        res.write('<body><h1>Welcome Home</h1></body>')
+        res.write('</html>')
+        res.end();
+    }
+    else if(req.url === '/about'){
+        res.setHeader('Content-type','text/html')
+        res.write('<html>');
+        res.write('<head></head>')
+        res.write('<body><h1>Welcome to About Us Page</h1></body>')
+        res.write('</html>')
+        res.end();
+    }
+    else if(req.url === '/node'){
+        res.setHeader('Content-type','text/html')
+        res.write('<html>');
+        res.write('<head></head>')
+        res.write('<body><h1>Welcome to our Node Project</h1></body>')
+        res.write('</html>')
+        res.end();
+    }
+    else{
+        res.setHeader('Content-type','text/html')
+        res.write('<html>');
+        res.write('<head></head>')
+        res.write('<body><h1>WRONG DOMAIN NAME</h1></body>')
+        res.write('</html>')
+        res.end();
+    }
 })
 server.listen(4000)
