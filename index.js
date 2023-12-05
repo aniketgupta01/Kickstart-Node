f9dcb5669fa140534c6c8ddb0dfbe836753602e4
aniketgupta01 <aniketg2831@gmail.com>
2023-12-05 19:41:35 +0530

    read/write file

--- index.js
+++ index.js
@@ -1,36 +1,44 @@
-const http= require('http');
-const server=http.createServer((req,res)=>{
-    if(req.url === '/home'){
-        res.setHeader('Content-type','text/html')
-        res.write('<html>');
-        res.write('<head></head>')
-        res.write('<body><h1>Welcome Home</h1></body>')
-        res.write('</html>')
-        res.end();
-    }
-    else if(req.url === '/about'){
-        res.setHeader('Content-type','text/html')
-        res.write('<html>');
-        res.write('<head></head>')
-        res.write('<body><h1>Welcome to About Us Page</h1></body>')
-        res.write('</html>')
-        res.end();
-    }
-    else if(req.url === '/node'){
-        res.setHeader('Content-type','text/html')
-        res.write('<html>');
-        res.write('<head></head>')
-        res.write('<body><h1>Welcome to our Node Project</h1></body>')
-        res.write('</html>')
-        res.end();
-    }
-    else{
-        res.setHeader('Content-type','text/html')
-        res.write('<html>');
-        res.write('<head></head>')
-        res.write('<body><h1>WRONG DOMAIN NAME</h1></body>')
-        res.write('</html>')
-        res.end();
-    }
-})
-server.listen(4000)
+const http = require("http");
+const fs = require("fs");
+const server = http.createServer((req, res) => {
+  const url = req.url;
+  const method = req.method;
+  if (url === "/") {
+    fs.readFile("message.txt", { encoding: "utf-8" }, (err, data) => {
+      if (err) {
+        console.log(err);
+      }
+
+      res.write("<html>");
+      res.write("<head></head>");
+      res.write(`<body>${data}</body>`);
+      res.write(
+        '<body><form action="/message" method="POST"><input type = "text" name="message"></input><button type="submit">Send</button></form></body>'
+      );
+      res.write("</html>");
+      return res.end();
+    });
+  }
+  else if (url === "/message" && method === "POST") {
+    const body = [];
+    req.on("data", (chunk) => {
+      body.push(chunk);
+    });
+    return req.on("end", () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split("=")[1];
+      fs.writeFile("message.txt", message, () => {
+        res.statusCode = 302;
+        res.setHeader("Location", "/");
+        return res.end();
+      });
+    });
+  }
+
+//   res.write("<html>");
+//   res.write("<head></head>");
+//   res.write("<body><h1>Welcome to About Us Page</h1></body>");
+//   res.write("</html>");
+//   res.end();
+});
+server.listen(4000);
