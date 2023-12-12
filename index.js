38fb6d65fdabddba98fba441d2caf0b93fc5aa97
aniketgupta01 <aniketg2831@gmail.com>
2023-12-12 15:16:45 +0530

    express

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
-const http= require('http');
-const routs=require('./routes');
 
-const server=http.createServer(routs);
 
-server.listen(3000)
+const express = require('express');
+const app = express();
+
+app.use((req,res,next)=>{
+    console.log('In a middleware')
+    next();
+})
+app.use((req,res,next)=>{
+    console.log('In another middleware')
+    res.setHeader('Content-Type','text/html')
+    res.send( '{ key1: value }')
+})
+
+
+app.listen(3000);
