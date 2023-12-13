fb77ac7f706ba2a73f55f78c352767f0360bd012
aniketgupta01 <aniketg2831@gmail.com>
2023-12-13 11:13:44 +0530

    middlewares

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
+const express = require("express");
+const bodyParser = require('body-parser')
 
-
-const express = require('express');
 const app = express();
 
-app.use((req,res,next)=>{
-    console.log('In a middleware')
-    next();
-})
-app.use((req,res,next)=>{
-    console.log('In another middleware')
-    res.setHeader('Content-Type','text/html')
-    res.send( '{ key1: value }')
-})
+app.use(bodyParser.urlencoded({extended:false}))
 
+app.use("/add-product", (req, res, next) => {
+  res.send(
+    '<form action="/product" method="POST"><input type="text" name="title" placeholder="Title"></input><input type="number" name="size" placeholder="Size"></input><button type="submit">Add Item</button></form>'
+  );
+});
+app.use("/product", (req, res, next) => {
+  console.log(req.body);
+  res.redirect("/");
+});
+
+app.use("/", (req, res, next) => {
+  res.send("<h1>Welcome to Express JS</h1>");
+});
 
 app.listen(3000);
